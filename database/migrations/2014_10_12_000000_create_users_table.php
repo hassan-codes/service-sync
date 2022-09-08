3ddd7ca718f560ddcfae6565767138d4131e5c4c
Baako Alhsassan Mohammed <baakomohammed95@gmail.com>
2022-09-08 06:10:52 +0000

    Modify users table schema

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role');
+            $table->boolean('is_active')->default(true);
+            $table->timestamp('password_reset_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
