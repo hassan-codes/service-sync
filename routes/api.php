f6b46722d47e430052252eff6b967fef8c30b42d
Baako Alhsassan Mohammed <baakomohammed95@gmail.com>
2022-09-08 03:58:26 +0000

    Creeate routes for login

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\v1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::post('v1/auth/login', [AuthController::class, 'login']);
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'v1/auth'
+], function ($router) {
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
