c4d5116053c72879d32d9400a0e29e68e2ff4565
Baako Alhsassan Mohammed <baakomohammed95@gmail.com>
2022-09-08 06:12:40 +0000

    Create AdminController

---
+++ app/Http/Controllers/v1/AdminController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\v1\StoreAdminRequest;
+use App\Models\User;
+use App\Services\v1\AdminService;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    protected AdminService $adminService;
+
+    public function __construct(AdminService $adminService)
+    {
+        $this->adminService = $adminService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StoreAdminRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAdminRequest $request)
+    {
+        return $this->adminService->invite($request);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
