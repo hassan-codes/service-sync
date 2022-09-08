f24a68f23b02126b7c2c34dff2225e718f366275
Baako Alhsassan Mohammed <baakomohammed95@gmail.com>
2022-09-08 18:23:00 +0000

    Refactor Admin service

--- app/Services/v1/AdminService.php
+++ app/Services/v1/AdminService.php
@@ -4,6 +4,7 @@ namespace App\Services\v1;
 
 use App\Http\Requests\v1\StoreAdminRequest;
 use App\Http\Requests\v1\UpdateAdminRequest;
+use App\Http\Resources\v1\AdminResource;
 use App\Models\User;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Hash;
@@ -61,7 +62,21 @@ class AdminService
 
     public function deactivate(int $userId)
     {
-        $user = User::where('id', $userId)->first();
+        $user = auth()->user();
+
+        if (! $this->isPasswordReset($user)) {
+            return response()->json([
+                'message' => 'Unauthorized. You must reset your password',
+            ], 401);
+        }
+
+        if (! $this->isAdmin($user)) {
+            return response()->json([
+                'message' => 'Unauthorized. Illegal action attempted',
+            ], 401);
+        }
+
+        $admin = User::where('id', $userId)->first();
         try {
             User::where('id', $userId)
                 ->where('role', 'admin')
@@ -72,34 +87,88 @@ class AdminService
             // TODO: email error to sysadmin
             return response()->json([
                 'error' => 'Failed to deactivate',
-            ], 500);
+            ], 422);
         }
 
         return response()->json([
-            'message' => 'Successfully deactivated account of ' . $user->last_name,
+            'message' => 'Successfully deactivated account of ' . $admin->last_name,
         ], 200);
     }
 
     public function update(UpdateAdminRequest $request, int $id)
     {
-        $user = User::where('id', $id)->first();
+        $user = auth()->user();
+
+        if (! $this->isPasswordReset($user)) {
+            return response()->json([
+                'message' => 'Unauthorized. You must reset your password',
+            ], 401);
+        }
+
+        if (! $this->isAdmin($user)) {
+            return response()->json([
+                'message' => 'Unauthorized. Illegal action attempted',
+            ], 401);
+        }
+
         try{
-            User::where('id', $id)
+            $admin = User::where('id', $id)
                 ->where('role', 'admin')
                 ->update([
                     'first_name' => strtoupper($request->first_name),
                     'last_name' => strtoupper($request->last_name),
                     'email' => strtolower($request->email)
                 ]);
+            $admin = User::where('id', $id)->first();
         } catch (QueryException $exception) {
             error_log($exception->getMessage());
             // TODO: email error to sysadmin
             return response()->json([
                 'error' => 'Failed to update',
-            ], 500);
+            ], 422);
         }
 
-        return $user;
+        return $admin;
+    }
+
+    public function fetch(int $userId = 0)
+    {
+        $user = auth()->user();
+
+        if (! $this->isPasswordReset($user)) {
+            return response()->json([
+                'message' => 'Unauthorized. You must reset your password',
+            ], 401);
+        }
+
+        if (! $this->isAdmin($user)) {
+            return response()->json([
+                'message' => 'Unauthorized. Illegal action attempted',
+            ], 401);
+        }
+
+        try {
+            $agents = AdminResource::collection(User::where('role', 'admin')
+                ->orderBy('first_name', 'ASC')
+                ->orderBy('last_name', 'ASC')
+                ->paginate());
+
+            if ($userId !== 0) {
+                $agents = AdminResource::collection(User::where('role', 'admin')
+                    ->where('id', $userId)
+                    ->orderBy('first_name', 'ASC')
+                    ->orderBy('last_name', 'ASC')
+                    ->paginate());
+            }
+        } catch (QueryException $exception) {
+            error_log($exception->getMessage());
+            // TODO: email error to sysadmin
+            return response()->json([
+                'error' => 'Failed to list agents',
+            ], 422);
+        }
+
+        return $agents;
     }
 
     protected function isPasswordReset(User $user)
