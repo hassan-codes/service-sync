782400a7da777a5f50082ecc9bc1e850ceaaa0a9
Baako Alhsassan Mohammed <baakomohammed95@gmail.com>
2022-09-10 16:42:49 +0000

    Create Transaction migration

---
+++ database/migrations/2022_09_10_125542_create_transactions_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->string('id', 13)->primary();
+            $table->decimal('amount',62,2);
+            $table->string('currency', 3);
+            $table->enum('type', ['CREDIT', 'DEBIT']);
+            $table->string('description', 2048);
+            $table->enum('status', ['APPROVED', 'REJECTED'])->nullable();
+            $table->string('reference')->nullable();
+            $table->unsignedBigInteger('posted_by');
+            $table->timestamp('posted_at');
+            $table->timestamp('fulfilled_at')->nullable();
+
+            $table->foreign('posted_by')
+                ->references('id')
+                ->on('Users')
+                ->onDelete('restrict')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_models');
+    }
+};
