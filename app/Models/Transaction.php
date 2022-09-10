65be24513243a76f4358f1258149a912dee98f32
Baako Alhsassan Mohammed <baakomohammed95@gmail.com>
2022-09-10 17:54:53 +0000

    Fix function scope

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -30,8 +30,9 @@ class Transaction extends Model
     ];
 
     public $timestamps = false;
-}
+
     public function user()
     {
         return $this->belongsTo(User::class, 'posted_by', 'id');
     }
+}
