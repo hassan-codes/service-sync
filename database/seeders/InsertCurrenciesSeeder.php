750a1141e235c48a01cbe5dcf0e79a88789ee52c
Baako Alhsassan Mohammed <baakomohammed95@gmail.com>
2022-09-10 17:54:20 +0000

    Create seeder to insert currencies

---
+++ database/seeders/InsertCurrenciesSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class InsertCurrenciesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $path = base_path('database/seeders/insertCurrencies.sql');
+        $sql = file_get_contents($path);
+        DB::unprepared($sql);
+    }
+}
