d1739a04cccdc647e28804a8ee388f640493090b
Baako Alhsassan Mohammed <baakomohammed95@gmail.com>
2022-09-18 14:55:02 +0000

    Customize password reset email message

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -45,7 +45,7 @@ class ResetPasswordNotification extends Notification
         return (new MailMessage)
                     ->greeting('Hello!')
                     ->line('You are receiving this email because we received a password reset request for your account.')
-                    ->action('Reset password', url('/'))
+                    ->action('Reset password', $this->url)
                     ->line('If you did not request a password reset, no further action is required.');
     }
 
